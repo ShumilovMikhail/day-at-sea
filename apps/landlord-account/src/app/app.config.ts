3cdf4e7363d525a84d3b81711529c412f37fab00
Mihail Shumilov <misha.shumilov1327@gmail.com>
2024-06-07 21:43:29 +0300

    [feature] add http library

--- apps/landlord-account/src/app/app.config.ts
+++ apps/landlord-account/src/app/app.config.ts
@@ -3,6 +3,9 @@ import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { provideStore } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
+import { API_URL } from '@http';
+import { environment } from '../environments/environment.development';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -10,6 +13,11 @@ export const appConfig: ApplicationConfig = {
     provideEffects(),
     provideStore(),
     provideZoneChangeDetection({ eventCoalescing: true }),
+    provideHttpClient(),
     provideRouter(appRoutes),
+    {
+      provide: API_URL,
+      useValue: environment.API_URL,
+    },
   ],
 };
---
+++ apps/landlord-account/src/environments/environment.development.ts
@@ -0,0 +1,3 @@
+export const environment = {
+  API_URL: 'https://x8ki-letl-twmt.n7.xano.io/api:iKo5T8gc',
+};
