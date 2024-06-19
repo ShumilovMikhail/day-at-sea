f822fcaf60e72dd558e457bffcc182dac64f3d70
Mihail Shumilov <misha.shumilov1327@gmail.com>
2024-06-19 18:54:57 +0300

    fix(latin-letters.validator)

--- libs/core/utils/util-validators/src/lib/util-validators/latin-letters.validator.ts
+++ libs/core/utils/util-validators/src/lib/util-validators/latin-letters.validator.ts
@@ -1,7 +1,7 @@
 import { AbstractControl, ValidatorFn, ValidationErrors } from '@angular/forms';
 
 export function latinLettersValidator(): ValidatorFn {
-  const pattern = /^[a-zA-Z]+[^а-яА-ЯёЁ]*$/;
+  const pattern = /^(?!.*[а-яА-ЯёЁ]).*$/;
   return (control: AbstractControl): ValidationErrors | null => {
     if (control.value.length === 0) {
       return null;
