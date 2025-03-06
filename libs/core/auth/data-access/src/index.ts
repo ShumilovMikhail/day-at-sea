6cb561ce08d4d0f1000512fc03de02b11de331c4
Mihail Shumilov <misha.shumilov1327@gmail.com>
2025-03-06 17:47:49 +0300

    refact(auth.guard) transfer to signal

--- libs/core/auth/data-access/src/index.ts
+++ libs/core/auth/data-access/src/index.ts
@@ -1,6 +1,5 @@
 export * from './lib/validators/username-available';
 export * from './lib/validators/email-available';
-export * from './lib/services/auth.facade';
 export * from './lib/services/user.facade';
 export * as authEffects from './lib/+state/auth.effects';
 export * from './lib/+state/auth.reducer';
--- libs/core/auth/data-access/src/lib/services/auth.guard.ts
+++ libs/core/auth/data-access/src/lib/services/auth.guard.ts
@@ -2,13 +2,14 @@ import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { Observable, tap } from 'rxjs';
 
-import { AuthFacade } from './auth.facade';
+import { AuthFacadeSignal } from './auth.facade-signal';
+import { toObservable } from '@angular/core/rxjs-interop';
 
 export function authGuard(): CanActivateFn {
   return (): Observable<boolean> => {
-    const authFacade = inject(AuthFacade);
+    const authFacade = inject(AuthFacadeSignal);
     const router = inject(Router);
-    return authFacade.isAuthenticate$.pipe(
+    return toObservable(authFacade.isAuthenticate$).pipe(
       tap((isAuthenticate) => {
         if (isAuthenticate) {
           return;
